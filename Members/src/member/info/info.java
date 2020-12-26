617c8d8e89d1c75d7d577a80188e627eea8c5ab3
Berachah Kwarteng-Siaw <emerson_siaw@yahoo.com>
2020-12-26 10:08:59 -0600

    Initial Commit

---
+++ Members/src/member/info/info.java
@@ -0,0 +1,51 @@
+package member.info;
+
+public class info {
+
+	
+	private   int id=0; 
+	private String name;
+	private String address;
+	private double donate;
+	
+	public int getId()
+	{
+		return id;
+	}
+	
+	public void setId(int id)
+	{
+		this.id=id;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+	
+	public String getAddress()
+	{
+		return address;
+	}
+	
+	public void setAddress( String address)
+	{
+		this.address=address;
+	}
+	
+	public double getDonation()
+	{
+		return donate;
+	}
+	
+	public void setDonation(double donate) {
+		this.donate=donate;
+	}
+
+	
+}
